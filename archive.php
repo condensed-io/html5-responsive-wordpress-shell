6d07d9f9b6effc9bff0b98114627b8e5b020cd9c
Brent Lagerman <brent@mimoymima.com>
2012-09-02 16:56:52 -0400

    updating the method used for if statements to be more readable

--- archive.php
+++ archive.php
@@ -19,21 +19,29 @@ if ($disableSidebarMain !== 'true'): ?>
 	<?php if (have_posts()) : ?>
 
 		<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-		<?php /* If this is a category archive */ if (is_category()) { ?>
+		
+		<?php if (is_category()) : // If this is a category archive ?>
 		<h1 class="page-title">Archive for &#8216;<?php single_cat_title(); ?>&#8217;</h1>
-		<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
+		
+		<?php elseif (is_tag()) : // If this is a tag archive  ?>
 		<h1 class="page-title">Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h1>
-		<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
+
+		<?php elseif (is_day()) : // If this is a daily archive ?>
 		<h1 class="page-title">Archive for <?php the_time('F jS, Y'); ?></h1>
-		<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+
+		<?php elseif (is_month()) : // If this is a monthly archive ?>
 		<h1 class="page-title">Archive for <?php the_time('F, Y'); ?></h1>
-		<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+
+		<?php elseif (is_year()) : // If this is a yearly archive ?>
 		<h1 class="page-title">Archive for <?php the_time('Y'); ?></h1>
-		<?php /* If this is an author archive */ } elseif (is_author()) { ?>
+
+		<?php elseif (is_author()) : // If this is an author archive ?>
 		<h1 class="page-title">Author Archive</h1>
-		<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-		<h1 class="page-title">Blog Archives</h1>	
-		<?php } ?>
+
+		<?php elseif (isset($_GET['paged']) && !empty($_GET['paged'])) : // If this is a paged archive ?>
+		<h1 class="page-title">Blog Archives</h1>
+
+		<?php endif; ?>
 	
 		<!--BEGIN: Archive-->
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
