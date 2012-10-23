f6a34a174607e8f4ae11d3c4df89a9c9cef418e4
Brent Lagerman <brent@mimoymima.com>
2012-10-23 04:41:47 -0400

    removing linked css from 503 template and instead having it within style tags in the head
    The 503 is used a lot as a coming soon page when a wordpress site is
    being developed so you don't usually want all the site styles getting
    inherited here.

--- 503.php
+++ 503.php
@@ -12,30 +12,16 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 
 	<title><?php if (is_single() || is_page() || is_archive()) { wp_title('',true); } else { bloginfo('description'); } ?> &#8212; <?php bloginfo('name'); ?></title>
-	
-	<!-- Meta Tags Handled by All In One SEO -->
-	
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/basic.css" media="all" />
-	<!--link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/print.css" media="print" /-->
-	<!--link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/handheld.css" media="handheld" /-->
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" media="screen" />
-
-	<?php $iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone"); ?>
-	<?php if ($iphone == true) : ?>
-		<link href="<?php bloginfo('template_url'); ?>/css/iphone.css" type="text/css" rel="stylesheet" />
-		<!--meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" /-->
-	<?php endif; ?>
-		
-	<!--BEGIN: IE Specific Stylesheets-->
-	<!--[if IE 8]><link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/ie8.css" media="screen" /><![endif]-->
-	<!--[if IE 7]><link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/ie7.css" media="screen" /><![endif]-->
-	<!--[if lt IE 7]><link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/ie6.css" media="screen" /><![endif]-->
-	<!--END: IE Specific Stylesheets-->
 
 	<link rel="copyright" href="#copyright" /> 
 	<link rel="icon" href="/favicon.ico" type="image/x-icon" />  
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-		
+	
+	<!-- add your 503 (maintenance or coming soon page) styles below -->
+	<style>
+		html, body { background: #EEE; color: #333; }
+	</style>
+
 </head>
 
 <body id="splash" class="maintenance">
