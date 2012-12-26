e2076244680ec6101dec060f8c68fedc7bc3c84b
Brent Lagerman <brent@mimoymima.com>
2012-12-26 14:02:39 -0500

    removing user agent sniffing for iphone/ipad/android
    we had the viewport meta tag only showing up for some devices but
    that's not necessary, cleans up the header.php

--- header.php
+++ header.php
@@ -1,10 +1,3 @@
-<?php
-// store a few user agent variables, don't delete, we'll use these later
-$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
-$ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
-$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
-?>
-
 <!DOCTYPE html>
 
 <html <?php language_attributes(); ?> class="no-js" xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://ogp.me/ns#">
@@ -37,12 +30,7 @@ $android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
 	<!--END: IE Specific Hacks-->
 	
 	<!--REMOVE this viewport code if you are making a site that is NOT responsive-->
-	<?php if ($iphone == true) : // conditional iPhone and Android code ?>
-		<link href="<?php echo $templateURL; ?>/css/iphone.css" type="text/css" rel="stylesheet" />
-		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
-	<?php elseif ($android == true) : ?>
-		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
-	<?php endif; ?>
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
 	<!--REMOVE this viewport code if you are making a site that is NOT responsive-->
 	
 	<?php wp_head(); // wp_head hook for Plugins ~ always keep this just before the /head tag ?>
