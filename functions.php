511b9bab1d6b7d7d0b1ff428e23589c02141d4cc
Brent Lagerman <brent@mimoymima.com>
2012-12-29 16:25:48 -0500

    removing the comment functions and instead linking to the article that describes how to use them
    most sites we make end up using LiveFyre, Disqus, or Intense Debate
    anyway, so it's a lot of wasted code if it's not goign to be used...

--- functions.php
+++ functions.php
@@ -116,67 +116,16 @@ function get_the_custom_excerpt($length){
 		}
 	}
 	
-// Custom callback to list comments in the your-theme style
-// http://themeshaper.com/2009/07/01/wordpress-theme-comments-template-tutorial/
-function custom_comments($comment, $args, $depth) {
-  $GLOBALS['comment'] = $comment;
-    $GLOBALS['comment_depth'] = $depth;
-  ?>
-    <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-        <div class="comment-author vcard"><?php commenter_link() ?></div>
-        <div class="comment-meta"><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'your-theme'),
-                    get_comment_date(),
-                    get_comment_time(),
-                    '#comment-' . get_comment_ID() );
-                    edit_comment_link(__('Edit', 'your-theme'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
-  <?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'your-theme') ?>
-          <div class="comment-content">
-            <?php comment_text() ?>
-        </div>
-        <?php // echo the comment reply link
-            if($args['type'] == 'all' || get_comment_type() == 'comment') :
-                comment_reply_link(array_merge($args, array(
-                    'reply_text' => __('Reply','your-theme'),
-                    'login_text' => __('Log in to reply.','your-theme'),
-                    'depth' => $depth,
-                    'before' => '<div class="comment-reply-link">',
-                    'after' => '</div>'
-                )));
-            endif;
-        ?>
-<?php } // end custom_comments
-
-// Custom callback to list pings
-function custom_pings($comment, $args, $depth) {
-       $GLOBALS['comment'] = $comment;
-        ?>
-            <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-                <div class="comment-author"><?php printf(__('By %1$s on %2$s at %3$s', 'your-theme'),
-                        get_comment_author_link(),
-                        get_comment_date(),
-                        get_comment_time() );
-                        edit_comment_link(__('Edit', 'your-theme'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
-    <?php if ($comment->comment_approved == '0') _e('\t\t\t\t\t<span class="unapproved">Your trackback is awaiting moderation.</span>\n', 'your-theme') ?>
-            <div class="comment-content">
-                <?php comment_text() ?>
-            </div>
-<?php } // end custom_pings
-
-// Produces an avatar image with the hCard-compliant photo class
-function commenter_link() {
-    $commenter = get_comment_author_link();
-    if ( ereg( '<a[^>]* class=[^>]+>', $commenter ) ) {
-        $commenter = ereg_replace( '(<a[^>]* class=[\'"]?)', '\\1url ' , $commenter );
-    } else {
-        $commenter = ereg_replace( '(<a )/', '\\1class="url "' , $commenter );
-    }
-    $avatar_email = get_comment_author_email();
-    $avatar = str_replace( "class='avatar", "class='photo avatar", get_avatar( $avatar_email, 80 ) );
-    echo $avatar . ' <span class="fn n">' . $commenter . '</span>';
-} // end commenter_link
-
-
 // To REMOVE unused dashboard widgets you can uncomment the next line and customize /includes/remove.php
 // require_once('includes/remove.php');
+
+/*
+COMMENT FUNCTIONS:
+we usually use LiveFyre, Disqus, or Intense Debate for comments
+also jetpack has some kind of commenting plugin that we haven't tried yet
+if you're making a site that requires a totally custom comments area,
+check this tutorial which has a bunch of functions to customize comments:
+http://themeshaper.com/2009/07/01/wordpress-theme-comments-template-tutorial/
+*/
 
 ?>
